92f5e226cbc7b7be2f52a86adf40b3a363feb056
Konstantin <grahovsky@gmail.com>
2019-03-02 23:08:23 +0300

    #retrieveMessages #messagesDB.observe(.childAdded) #let snapshotValue = snapshot.value as! Dictionary<String,String>

--- FlashChat/Controller/ChatViewController.swift
+++ FlashChat/Controller/ChatViewController.swift
@@ -20,6 +20,7 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet var messageTextfield: UITextField!
     @IBOutlet var messageTableView: UITableView!
     
+    var messageArray: [Message] = [Message]()
     
     
     override func viewDidLoad() {
@@ -43,6 +44,7 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         messageTableView.register(UINib(nibName: "CustomMessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
         
         configureTableView()
+        retrieveMessages()
         
     }
     
@@ -56,9 +58,9 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
         
-        let messageArray = ["First Message", "Second Message", "Third Message"]
-        
-        cell.messageBody.text = messageArray[indexPath.row]
+        cell.messageBody.text = messageArray[indexPath.row].messageBody
+        cell.senderUsername.text = messageArray[indexPath.row].sender
+        cell.avatarImageView.image = UIImage(named: "egg")
         
         return cell
     }
@@ -66,7 +68,7 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     //TODO: Declare numberOfRowsInSection here:
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return messageArray.count
     }
     
     
@@ -148,6 +150,28 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     //TODO: Create the retrieveMessages method here:
     
+    func retrieveMessages() {
+     
+        let messagesDB = Database.database().reference().child("Messages")
+        
+        messagesDB.observe(.childAdded) { (snapshot) in
+            let snapshotValue = snapshot.value as! Dictionary<String,String>
+            
+            let text = snapshotValue["MessageBody"]!
+            let sender = snapshotValue["Sender"]!
+            
+            print(text, sender)
+            
+            let newMesage = Message()
+            newMesage.sender = sender
+            newMesage.messageBody = text
+            
+            self.messageArray.append(newMesage)
+            self.configureTableView()
+            self.messageTableView.reloadData()
+        }
+        
+    }
     
     
     
